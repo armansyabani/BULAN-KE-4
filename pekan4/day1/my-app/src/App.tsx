4a9282ddd3d5073c1c4124f6a59087ae2f1371d1
armansyabani <armansyabni@gmail.com>
2026-01-29 00:18:04 +0700

    feat: initialize React app with Vite, add Counter and UserProfile components
    - Created package.json with dependencies and scripts for Vite and ESLint.
    - Added Vite logo SVG to public directory.
    - Created App.css for styling the application.
    - Implemented App component to include Counter and UserProfile components.
    - Developed Counter component with typed props and state management.
    - Created UserProfile component with complex state management using useReducer.
    - Added assets including React logo SVG.
    - Defined global styles in index.css.
    - Set up main entry point in main.tsx for rendering the app.
    - Configured Vite in vite.config.js for React support.

---
+++ pekan4/day1/my-app/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Counter from './components/Conter'
+import UserProfile from './components/UserProfile'
+
+function App() {
+
+
+  return (
+    <>
+   <Counter />
+   <UserProfile />
+    </>
+  )
+}
+
+export default App
